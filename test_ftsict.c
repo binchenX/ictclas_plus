52b249a362ee5e309fc4a8eba5a400aa5e0d0444
pierrchen <pierr.chen@gmail.com>
2011-07-11 19:37:56 +0800

    test_ftsctl works , be able to search

--- test_ftsict.c
+++ test_ftsict.c
@@ -25,32 +25,35 @@ sqlite3 * _dbHandle = NULL;
 
 int insert_one_row(const char * line ,int len)
 {
-int rc;
-sqlite3_stmt *sql_stmt;
-char *sql;
+    int rc;
+    sqlite3_stmt *sql_stmt;
+    char *sql;
 //sql = (char *)"INSERT INTO p2 VALUES('\u65b0\u534e\u7f51\u534e\u76db\u987f2\u670814\u65e5\u7535')";
-sql = (char *)"INSERT INTO p2 VALUES (?)";
+    sql = (char *)"INSERT INTO p2 VALUES (?)";
 
-rc = sqlite3_prepare_v2(_dbHandle,sql,-1,&sql_stmt,NULL);
+    rc = sqlite3_prepare_v2(_dbHandle,sql,-1,&sql_stmt,NULL);
 
-if (rc != SQLITE_OK) {
-    printf("Error when prepare :%s\n",sqlite3_errmsg(_dbHandle));
-    DB_ASSERT(!"did not call init_db() OR sql did not match table format OR bad sql");
-}
+    if (rc != SQLITE_OK)
+    {
+        printf("Error when prepare :%s\n",sqlite3_errmsg(_dbHandle));
+        DB_ASSERT(!"did not call init_db() OR sql did not match table format OR bad sql");
+    }
 
-rc = sqlite3_bind_text(sql_stmt,1, line, 40 ,SQLITE_TRANSIENT);
+    rc = sqlite3_bind_text(sql_stmt,1, line, 40 ,SQLITE_TRANSIENT);
 
-if(rc != SQLITE_OK) {
-    printf("Error when bin :%s\n",sqlite3_errmsg(_dbHandle));
-}
+    if (rc != SQLITE_OK)
+    {
+        printf("Error when bin :%s\n",sqlite3_errmsg(_dbHandle));
+    }
 
-rc = sqlite3_step(sql_stmt);
+    rc = sqlite3_step(sql_stmt);
 
-if(rc != SQLITE_DONE) {
-    printf("Error when insert :%s\n",sqlite3_errmsg(_dbHandle));
-}
-sqlite3_finalize(sql_stmt);
-return sqlite3_last_insert_rowid(_dbHandle);
+    if (rc != SQLITE_DONE)
+    {
+        printf("Error when insert :%s\n",sqlite3_errmsg(_dbHandle));
+    }
+    sqlite3_finalize(sql_stmt);
+    return sqlite3_last_insert_rowid(_dbHandle);
 }
 
 
@@ -94,10 +97,24 @@ void get_oneline(char *oBuff, int *oLen)
     assert(ret == fileSize);
 
     *oLen = fileSize;
+    {
+        //output the bytes.........
+        int i = 0;
+        for (i = 0 ;i < fileSize ;i++)
+        {
+            printf("%x " , oBuff[i]);
+        }
+
+        printf("\n" , oBuff[i] & 0x0f);
+
+    }
 
     return;
 
 }
+
+
+int get_search_words(void);
 
 int main()
 {
@@ -110,15 +127,24 @@ int main()
 
     sqlite3_enable_load_extension(_dbHandle,1);
 
-    if(rc != SQLITE_OK){printf("Error :%s\n",sqlite3_errmsg(_dbHandle));}
+    if (rc != SQLITE_OK)
+    {
+        printf("Error :%s\n",sqlite3_errmsg(_dbHandle));
+    }
 
     rc = sqlite3_exec(_dbHandle,"SELECT load_extension('./libftsict.so')", NULL, 0, NULL);
-    if(rc != SQLITE_OK){printf("Error when loading extension :%s\n",sqlite3_errmsg(_dbHandle));}
+    if (rc != SQLITE_OK)
+    {
+        printf("Error when loading extension :%s\n",sqlite3_errmsg(_dbHandle));
+    }
 
     const char *sql_create_tblSection = "CREATE VIRTUAL TABLE p2 USING fts3(t, tokenize=ictclas)";
     //const char *sql_create_tblSection = "CREATE VIRTUAL TABLE p2 USING fts3(t)";
     rc = sqlite3_exec(_dbHandle,sql_create_tblSection,NULL,0,NULL);
-    if(rc != SQLITE_OK){printf("Error :%s\n",sqlite3_errmsg(_dbHandle));}
+    if (rc != SQLITE_OK)
+    {
+        printf("Error :%s\n",sqlite3_errmsg(_dbHandle));
+    }
 
     char line[1000];
 
@@ -134,6 +160,48 @@ int main()
 
     insert_one_row(line , len);
 
+    //search 
+    get_search_words();
+
+
     return 0;
+}
+
+
+int get_search_words(void)
+{
+    int rc;
+    int count = 0;
+    sqlite3_stmt *sql_stmt;
+    char *sql;
+    int i = 0 ;
+    sql = (char *)"SELECT t FROM p2 WHERE t MATCH ? ";
+
+    rc = sqlite3_prepare_v2(_dbHandle, sql, -1 , &sql_stmt, NULL);
+
+    char keywords[4] = {0xd0,0xc2,0xbb,0xaa};
+
+    rc = sqlite3_bind_text(sql_stmt,1, keywords, 4 ,SQLITE_TRANSIENT);
+
+    rc = sqlite3_step(sql_stmt); 
+    while (rc == SQLITE_ROW)
+    {
+        //Event_basic_t * p = &array[i++] ;
+        char text[1000];
+        memset(text, 0 , 1000);
+        //0.t
+        int len = sqlite3_column_bytes(sql_stmt,0);
+        memcpy( text,sqlite3_column_text(sql_stmt,0), len);
+        printf("find match %s \n" , text);
+
+        rc = sqlite3_step(sql_stmt);
+        count++;
+    } //end while()
+
+
+    printf("find %d match\n",count);
+    rc = sqlite3_finalize(sql_stmt);
+    return count;
+
 }
 
