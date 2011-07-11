b0d30c02f73322962603035f476b40dd96c9bf24
pierrchen <pierr.chen@gmail.com>
2011-07-11 16:24:32 +0800

    add test_ftsict.c

---
+++ test_ftsict.c
@@ -0,0 +1,25 @@
+#include "sqlite3.h"
+
+
+
+
+
+
+
+
+
+
+/*** 
+ 
+ create virtual FTS table using ictclas as the tokenzier.
+ insert some data and then issue the query
+ 
+ we can use this program to debug
+ 
+ */
+
+int main()
+{
+
+
+}
